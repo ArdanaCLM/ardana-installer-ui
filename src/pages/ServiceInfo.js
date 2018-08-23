e25a6d7e9a25a2cef18fa2fd558f5917ad487d19
Julie Gravel <julie.gravel@suse.com>
2018-08-23 11:26:04 -0700

    Add Run Status Playbook action for services (#133)
    SCRD-3485 Added a dropdown menu that contains 'Details' and 'Run Status Playbook'
    actions to each service in the Service Information page. Then implemented the modal
    that showed the streaming of result from running status playbook for the selected
    service. The Details menu doesn't do anything at the moment.

--- src/pages/ServiceInfo.js
+++ src/pages/ServiceInfo.js
@@ -17,21 +17,74 @@ import React, { Component } from 'react';
 import { translate } from '../localization/localize.js';
 import { fetchJson } from '../utils/RestUtils.js';
 import { alphabetically } from '../utils/Sort.js';
+import { PlaybookProgress } from '../components/PlaybookProcess.js';
+import { LoadingMask } from '../components/LoadingMask.js';
+import ContextMenu from '../components/ContextMenu.js';
 
 class ServiceInfo extends Component {
 
   constructor() {
     super();
     this.state = {
-      services: undefined
+      services: undefined,
+      showActionMenu: false,
+      showDetailsModal: false,
+      showRunStatusPlaybookModal: false,
+      playbooks: [],
+      steps: [],
+      selectedService: '',
+      showLoadingMask: false,
+      menuLocation: undefined
     };
   }
 
   componentWillMount() {
+    this.setState({showLoadingMask: true});
     fetchJson('/api/v1/clm/endpoints')
       .then(responseData => {
-        this.setState({services: responseData});
+        this.setState({services: responseData, showLoadingMask: false});
       });
+  }
+
+  renderActionMenuIcon = (service) => {
+    return (
+      <span onClick={(event) => this.handleActionMenu(event, service)}>
+        <i className='material-icons'>more_horiz</i>
+      </span>
+    );
+  }
+
+  handleActionMenu = (event, service) => {
+    this.setState({
+      showActionMenu: true,
+      selectedService: service,
+      menuLocation: {x: event.pageX, y: event.pageY}
+    });
+  }
+
+  showDetailsModal = () => {
+    this.setState({showActionMenu: false});
+  }
+
+  showRunStatusPlaybookModal = () => {
+    const playbookName = this.state.selectedService + '-status';
+    this.setState({
+      showActionMenu: false,
+      showRunStatusPlaybookModal: true,
+      playbooks: [playbookName],
+      steps: [{label: 'status', playbooks: [playbookName + '.yml']}],
+    });
+  }
+
+  renderMenuItems = () => {
+    const menuItems = [
+      {show: true, key: 'common.details', handleShowModal: this.showDetailsModal},
+      {show: true, key: 'services.run.status', handleShowModal: this.showRunStatusPlaybookModal},
+    ];
+    return (
+      <ContextMenu show={this.state.showActionMenu} items={menuItems} location={this.state.menuLocation}
+        close={() => this.setState({showActionMenu: false})}/>
+    );
   }
 
   render() {
@@ -43,7 +96,7 @@ class ServiceInfo extends Component {
           const regions = srv.endpoints.map(ep => {return ep.region;}).join('\n');
           const endpoints = srv.endpoints.map(ep => {
             // capitalize the interface before concat with the url
-            const types = ep.interface.charAt(0).toUpperCase() + ep.interface.substr(1);
+            const types = ep.interface[0].toUpperCase() + ep.interface.substr(1);
             return types + ' ' + ep.url;
           }).join('\n');
 
@@ -53,27 +106,44 @@ class ServiceInfo extends Component {
               <td>{srv.description}</td>
               <td className='line-break'>{endpoints}</td>
               <td className='line-break'>{regions}</td>
+              <td>{this.renderActionMenuIcon(srv.name)}</td>
             </tr>
           );
         });
     }
 
+    let statusModal;
+    if (this.state.showRunStatusPlaybookModal) {
+      const serviceName = this.state.selectedService[0].toUpperCase() + this.state.selectedService.substr(1);
+      statusModal = (
+        <PlaybookProgress steps={this.state.steps} playbooks={this.state.playbooks}
+          updatePageStatus={() => {}} modalMode showModal={this.state.showRunStatusPlaybookModal}
+          onHide={() => this.setState({showRunStatusPlaybookModal: false})} serviceName={serviceName}/>
+      );
+    }
+
     return (
-      <div className='menu-tab-content'>
-        <div className='header'>{translate('services.info')}</div>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>{translate('services.name')}</th>
-              <th>{translate('services.description')}</th>
-              <th>{translate('services.endpoints')}</th>
-              <th>{translate('services.regions')}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
+      <div>
+        {statusModal}
+        <LoadingMask show={this.state.showLoadingMask}></LoadingMask>
+        <div className='menu-tab-content'>
+          <div className='header'>{translate('services.info')}</div>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>{translate('name')}</th>
+                <th>{translate('description')}</th>
+                <th>{translate('endpoints')}</th>
+                <th>{translate('regions')}</th>
+                <th width="3em"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
+          {this.state.showActionMenu && this.renderMenuItems()}
+        </div>
       </div>
     );
   }
