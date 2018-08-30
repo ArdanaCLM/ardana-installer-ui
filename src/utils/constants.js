ffbdff4a4bddfc129f161c73c470dff0371ae575
Jack Choy <jack.likes.to.code@gmail.com>
2018-08-30 14:29:46 -0700

    SCRD-3455 Constant-ize cleanup some playbook names

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -47,3 +47,9 @@ export const IS_MS_EDGE = /Edge/.test(window.navigator.userAgent);
 export const IS_MS_IE = /MSIE\s|Trident\/7\./.test(window.navigator.userAgent);
 
 export const TRUTHY = [true, 'true', 'True', 'yes', 'Yes'];
+
+/** Playbooks **/
+export const INSTALL_PLAYBOOK = 'installui-os-provision';
+export const PRE_DEPLOYMENT_PLAYBOOK = 'installui-pre-deployment';
+export const DAYZERO_SITE_PLAYBOOK = 'installui-wipe-and-site';
+export const SITE_PLAYBOOK = 'site';
