ef4da3c5e253080ea0ff982941d7de6292a7dc27
Julie Gravel <julie.gravel@suse.com>
2018-09-10 15:36:49 -0700

    SCRD-3489 Add Update Service Configuration page - Part One (#153)

--- src/utils/RouteConfig.js
+++ src/utils/RouteConfig.js
@@ -18,6 +18,7 @@ import { translate } from '../localization/localize.js';
 import ServiceInfo from '../pages/ServiceInfo';
 import OpenStackPackages from '../pages/OpenStackPackages';
 import ArdanaPackages from '../pages/ArdanaPackages';
+import ServiceConfiguration from '../pages/ServiceConfiguration';
 import ServicesPerRole from '../pages/ServicesPerRole';
 import { SpeedometerTest } from '../pages/SpeedometerTest';
 import { AlarmDonutTest } from '../pages/AlarmDonutTest';
@@ -78,7 +79,7 @@ export const routes = [
       { name: translate('information'), slug: '/services/info', component: ServiceInfo },
       { name: translate('packages.openstack'), slug: '/services/openstack-packages', component: OpenStackPackages },
       { name: translate('packages.ardana'), slug: '/services/openstack-ardana', component: ArdanaPackages },
-      { name: translate('configure'), slug: '/services/configure', component: Example , unfinished: true },
+      { name: translate('configuration'), slug: '/services/configuration', component: ServiceConfiguration },
       { name: translate('roles'), slug: '/services/roles', component: ServicesPerRole },
     ]
   },
