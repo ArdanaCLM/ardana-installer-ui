1ab09de19177694488d833865e6003ab8fa008eb
Ethan Apodaca <eapodaca@suse.com>
2018-11-13 14:15:51 -0800

    Fix issue with pre-commit hook
    * Fix issue that prevents committing files that are not js

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,4 +3,7 @@
 exec 1>&2
 
 files_changed=$(git diff --cached --name-status | awk '$1 != "D" { print $2 }' | grep \.js$)
-[[ $files_changed ]] && node_modules/.bin/eslint $files_changed
+if [[ $files_changed ]]; then
+  node_modules/.bin/eslint $files_changed
+  exit $?
+fi
