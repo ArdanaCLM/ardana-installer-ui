fba79b4bdd07d1dab31825b896ea675ceef4b2fe
Ethan Apodaca <eapodaca@suse.com>
2018-11-13 12:52:13 -0800

    SCRD-5727 Refactor validators to only return error (#219)

--- src/components/ValidatingInput.js
+++ src/components/ValidatingInput.js
@@ -49,38 +49,27 @@ export class ValidatingInput extends Component {
   isRequired = () => TRUTHY.includes(this.props.isRequired) || TRUTHY.includes(this.props.required)
 
   validateInput(val, extraProps) {
-    let retValid = false;
-
-    if(this.isRequired()) {
-      if(val === undefined || val.length === 0) {
-        this.setState({errorMsg: translate('input.required.error')});
-        return retValid;
-      }
+    if(this.isRequired() && (val === undefined || val.length === 0)) {
+      this.setState({errorMsg: translate('input.required.error')});
+      return false;
     }
 
     if(this.props.inputValidate && val !== '' && val !== undefined) {//have a validator and have some values
-      let validateObject = this.props.inputValidate(val, extraProps);
-      if (validateObject) {
-        if(validateObject.isValid) {
-          this.setState({errorMsg: ''});
-          retValid = true;
-        }
-        else {
-          this.setState({
-            errorMsg: validateObject.errorMsg
-          });
-        }
-      }
-      else {
+      let err;
+      try {
+        err = this.props.inputValidate(val, extraProps);
+      } catch(e) {
         this.setState({errorMsg: translate('input.validator.error')});
+        return false;
       }
-    }
-    else {  //don't have validator
-      retValid = true;
-      this.setState({ errorMsg: ''});
+      if (err) {
+        this.setState({ errorMsg: err });
+        return false;
+      }
     }
 
-    return retValid;
+    this.setState({errorMsg: undefined});
+    return true;
   }
 
   handleInputChange = (e, props) => {
