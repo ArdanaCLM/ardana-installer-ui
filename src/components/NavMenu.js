269a691b4b5760305068a91a3c32926c5c4639d3
Jeremy Moffitt <jeremy.moffitt@suse.com>
2019-01-29 10:12:47 -0800

    SCRD-5795 - fix browser tab title for Day2 UI (#283)

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -86,9 +86,6 @@ class NavMenu extends Component {
 
     const logout_tooltip = (<Tooltip id='logout'>{translate('logout')}</Tooltip>);
 
-    // overwrite default tab title to use branding title
-    document.title = translate('day2.product.title');
-
     return(
       <Router>
         {/* Router requires a single child, so surround everything in a div */}
