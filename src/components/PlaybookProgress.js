06ecc200dadd249e262bcb1ed902648e247d497e
Gloria Gu <gfgu@suse.com>
2019-03-12 11:27:13 -0700

    Select specific nodes to wipe disks in day0 (SCRD6836)
    This commit includes:
    1. Added multiple select list to allow user select nodes when
    wipe disks checked.
    2. run site playbook and wipe disk playbook individually when
    wipe disks checked.

--- src/components/PlaybookProgress.js
+++ src/components/PlaybookProgress.js
@@ -597,6 +597,8 @@ class PlaybookProgress extends Component {
 
     } else {
       const playbookName = this.getPlaybookName(playbook);
+      //console.log('name=' + playbookName)
+      //console.log('payload=' + JSON.stringify(payload))
       postJson('/api/v2/playbooks/' + playbookName, payload)
         .then(response => {
           const playId = response['id'];
